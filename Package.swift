d2bafa252ac09d6d11128cb01b14e62baf86ec7e
Tomas Harkema <tomas@harkema.io>
2026-04-01 19:56:31 +0200

    fix flatpak

--- Package.swift
+++ Package.swift
@@ -7,29 +7,19 @@ let package = Package(
     name: "swift-discovery",
     platforms: [.macOS(.v15)],
     dependencies: [
-        .package(url: "https://github.com/makoni/swift-adwaita.git", branch: "main"),
+        .package(url: "https://github.com/makoni/swift-adwaita", branch: "main"),
         .package(url: "https://github.com/fwcd/swift-dns-service-discovery", branch: "main"),
-//        .package(url: "https://github.com/AparokshaUI/Localized", from: "0.2.0"),
-        // .package(url: "https://github.com/rhx/gir2swift", branch: "main"),
-        // .package(url: "https://github.com/rhx/SwiftGtk",  branch: "gtk4"),
-        
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "swift-discovery",
+            name: "Discovery",
             dependencies: [
                 .product(name: "Adwaita", package: "swift-adwaita"),
-               .product(name: "DNSServiceDiscovery", package: "swift-dns-service-discovery")
-//                .product(name: "Localized", package: "Localized"),
-                // .product(name: "Gtk", package: "SwiftGtk"),
+               .product(name: "DNSServiceDiscovery", package: "swift-dns-service-discovery"),
             ]
         ),
-        .testTarget(
-            name: "swift-discoveryTests",
-            dependencies: ["swift-discovery"]
-        )
     ]
 )
 
